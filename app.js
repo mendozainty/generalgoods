7a0fce27dc6bf86589ac0f684c33e87e832e533a
Inty Mendoza <intymendoza@gmail.com>
2022-03-25 19:29:47 -0300

    in proccess

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const https = require("https");
+const path = require("path");
+
+
+const index = require('./routes/index')
 
 const app = express();
 const port = 3000;
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html");
-});
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', index);
+
+
+// app.get("/", function(req, res){
+//   res.sendFile(__dirname + "/index.html");
+// });
+
 
 
 app.listen(port, ()=>{
---
+++ public/javascripts/scripts.js
@@ -0,0 +1,8 @@
+  $('.navTrigger').on("click", function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
