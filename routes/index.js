6d1dcf86f0247263ad3f48cbd74c6a035a9771a2
Inty Mendoza <intymendoza@gmail.com>
2022-04-06 13:19:06 -0300

    website ok

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET home */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express'})
-})
-
-module.exports = router;
